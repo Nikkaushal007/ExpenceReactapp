43257fe710e29f83e05f2ce4b6f7144dc38ca0a8
Nikkaushal007 <nikhilkaushal613@gmail.com>
2023-01-26 18:48:32 +0530

    first commit

---
+++ src/components/ExpenceItem.js
@@ -0,0 +1,13 @@
+function ExpenceItem() {
+return (
+    <div>
+    <div>Food Rs 10 </div>
+    <div>
+        <h2>Petrol Rs 100</h2>
+        <div>Movies Rs 200</div>
+    </div>
+    </div>
+);
+}
+
+export default ExpenceItem;
