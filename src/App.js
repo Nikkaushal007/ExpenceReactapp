e7239f28eab0e57cb52cf94738f033fa7b104953
Nikkaushal007 <nikhilkaushal613@gmail.com>
2023-01-28 19:15:10 +0530

    EXpense props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import ExpenceItem from './components/ExpenceItem';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  
+  const expenses = [
+    { title: 'Toilet Paper', amount: 200.55, date: new Date(2023, 1, 28)},
+    { title: 'Car Insurance', amount: 296.55, date: new Date(2023, 1, 28)},
+    { title: 'Bike Accessories', amount: 244.55, date: new Date(2023, 1, 28)},
+    { title: 'Books', amount: 225.55, date: new Date(2023, 1, 28)},
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <ExpenceItem></ExpenceItem>
+        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
         </p>
         <a
           className="App-link"
